92d70e8c2b50b3d85ff308a0689258d4e0fe4d26
Gabriel Nwankwo <gfunzy@gmail.com>
2025-11-27 10:38:58 +0100

    Tweaks

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -33,146 +33,103 @@
     <div class="py-6">
         <!-- row -->
         <div class="row">
-            {{-- @if (Auth::user()->profile_updated == 0) --}}
-                <div class="offset-xl-2 col-xl-8 col-md-12 col-12">
+
+            <div class="offset-xl-2 col-xl-8 col-md-12 col-12">
+
+                @if (Auth::user()->profile_updated == 0)
                     <div class="alert alert-danger">Please Update Your Profile Information.</div>
+                @endif
 
-                    <!-- card -->
-                    <div class="card">
-                        <!-- card body -->
-                        <div class="card-body p-lg-6">
-                            <!-- form -->
-                            <form method="post" action="{{ route('admin.updateProfile') }}"
-                                enctype="multipart/form-data">
-                                @csrf
-                                <div class="row">
-                                    <!-- form group -->
-                                    <div class="mb-3 col-md-6 col-12">
-                                        <label class="form-label">Last Name <span class="text-danger">*</span></label>
-                                        <input type="text" name="last_name" value="{{ Auth::user()->last_name }}"
-                                            class="form-control @error('last_name') is-invalid @enderror"
-                                            placeholder="Enter Last Name" required>
-                                        @error('last_name')
-                                            <span class="" role="alert">
-                                                <strong style="color: #b02a37; font-size:12px">{{ $message }}</strong>
+                <!-- card -->
+                <div class="card">
+                    <!-- card body -->
+                    <div class="card-body p-lg-6">
+                        <!-- form -->
+                        <form method="post" action="{{ route('admin.updateProfile') }}" enctype="multipart/form-data">
+                            @csrf
+                            <div class="row">
+                                <!-- form group -->
+                                <div class="mb-3 col-md-6 col-12">
+                                    <label class="form-label">Last Name <span class="text-danger">*</span></label>
+                                    <input type="text" name="last_name" value="{{ Auth::user()->last_name }}"
+                                        class="form-control @error('last_name') is-invalid @enderror"
+                                        placeholder="Enter Last Name" required>
+                                    @error('last_name')
+                                        <span class="" role="alert">
+                                            <strong style="color: #b02a37; font-size:12px">{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="mb-3 col-md-6 col-12">
+                                    <label class="form-label">Other Names <span class="text-danger">*</span></label>
+                                    <input type="text" name="other_names" value="{{ Auth::user()->other_names }}"
+                                        class="form-control @error('other_names') is-invalid @enderror"
+                                        placeholder="Enter Other Names" required>
+                                    @error('other_names')
+                                        <span class="" role="alert">
+                                            <strong style="color: #b02a37; font-size:12px">{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="mb-3 col-md-6 col-12">
+                                    <label class="form-label">Email<span class="text-danger">*</span></label>
+                                    <input type="email" name="email" value="{{ Auth::user()->email }}"
+                                        class="form-control @error('email') is-invalid @enderror"
+                                        placeholder="Enter Last Name" required readonly>
+                                    @error('email')
+                                        <span class="" role="alert">
+                                            <strong style="color: #b02a37; font-size:12px">{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="mb-3 col-md-6 col-12">
+                                    <label class="form-label">Phone Number<span class="text-danger">*</span></label>
+                                    <input type="text" name="phone_number" value="{{ Auth::user()->phone_number }}"
+                                        class="form-control @error('phone_number') is-invalid @enderror"
+                                        placeholder="Enter Phone Number" required>
+                                    @error('phone_number')
+                                        <span class="" role="alert">
+                                            <strong style="color: #b02a37; font-size:12px">{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-md-6 col-12 mb-4">
+                                    <div>
+                                        <!-- logo -->
+                                        <h5 class="mb-3">Profile Photo </h5>
+                                        <div class="icon-shape icon-xxl border rounded position-relative">
+                                            <span class="position-absolute">
+                                                <img alt="avatar"
+                                                    src="{{ Auth::user()->profile_photo == null ? asset('assets/images/avatar/avatar.webp') : Auth::user()->profile_photo }}"
+                                                    style="max-height:140px; max-width: 150px">
                                             </span>
-                                        @enderror
-                                    </div>
 
-                                    <div class="mb-3 col-md-6 col-12">
-                                        <label class="form-label">Other Names <span class="text-danger">*</span></label>
-                                        <input type="text" name="other_names" value="{{ Auth::user()->other_names }}"
-                                            class="form-control @error('other_names') is-invalid @enderror"
-                                            placeholder="Enter Other Names" required>
-                                        @error('other_names')
-                                            <span class="" role="alert">
-                                                <strong style="color: #b02a37; font-size:12px">{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-
-                                    <div class="mb-3 col-md-6 col-12">
-                                        <label class="form-label">Email<span class="text-danger">*</span></label>
-                                        <input type="email" name="email" value="{{ Auth::user()->email }}"
-                                            class="form-control @error('email') is-invalid @enderror"
-                                            placeholder="Enter Last Name" required readonly>
-                                        @error('email')
-                                            <span class="" role="alert">
-                                                <strong style="color: #b02a37; font-size:12px">{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                    <div class="mb-3 col-md-6 col-12">
-                                        <label class="form-label">Phone Number<span class="text-danger">*</span></label>
-                                        <input type="text" name="phone_number"
-                                            value="{{ Auth::user()->phone_number }}"
-                                            class="form-control @error('phone_number') is-invalid @enderror"
-                                            placeholder="Enter Phone Number" required>
-                                        @error('phone_number')
-                                            <span class="" role="alert">
-                                                <strong style="color: #b02a37; font-size:12px">{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-
-                                    <div class="col-md-6 col-12 mb-4">
-                                        <div>
-                                            <!-- logo -->
-                                            <h5 class="mb-3">Profile Photo </h5>
-                                            <div class="icon-shape icon-xxl border rounded position-relative">
-                                                <span class="position-absolute">
-                                                    <img alt="avatar"
-                                                        src="{{ Auth::user()->profile_photo == null ? asset('assets/images/avatar/avatar.webp') : Auth::user()->profile_photo }}"
-                                                        style="max-height:140px; max-width: 150px">
-                                                </span>
-
-
-                                            </div>
 
                                         </div>
-                                    </div>
-                                    <div class="col-md-6 col-12 mb-4">
-                                        <h5 class="mb-3">&nbsp; </h5>
-                                        <input type="file" name="profile_photo" class="form-control">
-                                    </div>
-                                    <div class="col-md-8"></div>
-                                    <!-- button -->
-                                    <div class="col-12">
-                                        <button class="btn btn-primary" type="button"
-                                            onClick="this.disabled=true; this.innerHTML='Submiting request, please wait...';this.form.submit();">Save
-                                            Changes</button>
 
                                     </div>
                                 </div>
-                            </form>
-                        </div>
+                                <div class="col-md-6 col-12 mb-4">
+                                    <h5 class="mb-3">&nbsp; </h5>
+                                    <input type="file" name="profile_photo" class="form-control">
+                                </div>
+                                <div class="col-md-8"></div>
+                                <!-- button -->
+                                <div class="col-12">
+                                    <button class="btn btn-primary" type="button"
+                                        onClick="this.disabled=true; this.innerHTML='Submiting request, please wait...';this.form.submit();">Save
+                                        Changes</button>
 
+                                </div>
+                            </div>
+                        </form>
                     </div>
+
                 </div>
-            {{-- @else
-                <div class="offset-xl-1 col-xl-10 col-md-12 col-12">
-                    <div class="card">
-                        <!-- card body -->
-                        <div class="card-body p-lg-6">
-
-                            <table class="table table-bordered">
-                                <tbody>
-                                    <tr>
-                                        <td class="">Last Name</td>
-                                        <td class="">{{ Auth::user()->last_name }}</td>
-                                        <td class="" rowspan="8" align="right" style="text-align: center"><img
-                                                src="{{ Auth::user()->profile_photo == null ? asset('assets/images/avatar/avatar.webp') : Auth::user()->profile_photo }}"
-                                                id="vphoto" class="img-responsive" style="max-width: 150px" />
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="">Other Names</td>
-                                        <td class="">{{ Auth::user()->other_names }}</td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="">Email Address</td>
-                                        <td class="">{{ Auth::user()->email }}</td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="">Phone Number</td>
-                                        <td class="">{{ Auth::user()->phone_number }}</td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="">Assigned User Role</td>
-                                        <td class="">{{ Auth::user()->userRole->role }}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-
-                        </div>
-
-                    </div>
-                </div>
-            @endif --}}
+            </div>
         </div>
     </div>
 </section>
